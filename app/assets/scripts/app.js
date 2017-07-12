7a70053c66d1df62a5236fcc3addd68f15f73989
Vedran <lotd11@gmail.com>
2017-07-12 21:47:01 +0200

    Completed header

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,21 @@
+import MobileMenu from './modules/MobileMenu';
+
+var mobileMenu = new MobileMenu();
+
+//var Person = require('./modules/person');
+// import Person from './modules/person';
+
+// class Adult extends Person {
+//     paytaxes() {
+//         console.log(this.name + ' now owes 0$ in taxes.');
+//     }
+// }
+
+// var john = new Person('John Doe', 'blue');
+// john.greet();
+
+// var jane = new Adult('Jane Smith', 'orange');
+// jane.greet();
+// jane.paytaxes();
+
+
---
+++ app/assets/scripts/modules/person.js
@@ -0,0 +1,23 @@
+// function Person(fullName, favColor) {
+//     this.name = fullName;
+//     this.favoriteColor = favColor;
+//     this.greet = function () {
+//         console.log('Hello, my name is ' + this.name + 'and my favorite color is ' + this.color);
+//     }
+// }
+
+// class Person {
+
+//     constructor(fullName, favColor) {
+//         this.name = fullName;
+//         this.favoriteColor = favColor;
+//     }
+
+//     greet() {
+//         console.log('Hi, my name is ' + this.name + 'and my favorite color is ' + this.favoriteColor);
+//     }
+// }
+
+// //module.exports = Person;
+
+// export default Person;
